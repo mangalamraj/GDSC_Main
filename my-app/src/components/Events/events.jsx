03e6151c89d783957652006bae2899b3c6ca0d22
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-06-20 16:36:40 +0530

    link

--- my-app/src/components/Events/events.jsx
+++ my-app/src/components/Events/events.jsx
@@ -3,9 +3,17 @@ import styles from "./events.module.css"
 
 const Events = () =>{
     return(
+        <div style={{
+            alignItems:"center",
+            textAlign:"center",
+            
+        }}>
+
+     
         <div className={styles.eventsParents} id="events">
             <div className={styles.mango}>
             <div className={styles.head}>Recent Events</div>
+
             <div className={styles.events}>
                 <Eventbox image="https://res.cloudinary.com/startup-grind/image/upload/c_limit,dpr_2,f_auto,g_center,h_1440,q_auto:good,w_2048/v1/gcs/platform-data-dsc/event_wrapup/WhatsApp%2520Image%25202023-04-03%2520at%252012.10.37%2520AM.jpeg" heading = "TechCon" para="Get ready to take your web development skills to the next level! Join us for an exciting workshop hosted by GDSC IIITN and gain hands-on experience in creating dynamic and responsive websites. Don't miss out on this opportunity to learn from experts and connect with fellow web enthusiasts. See you there!"></Eventbox>
                 <Eventbox image="images/hack.svg" heading = "InfoSession 2.0" para="INFO SESSION 1.0
@@ -16,6 +24,11 @@ GDSC-IIIT Nagpur brings you Hack-Droid, an Android based hackathon where you get
             </div>
             </div>
 
+        </div>
+        <div style={{alignItems:"center",textAlign:"center",marginTop:"-35px",display:"flex",justifyContent:"center",}}>
+        <a href="https://gdsc.community.dev/indian-institute-of-information-technology-nagpur/">View More</a>
+        </div>
+        
         </div>
     )
 }
