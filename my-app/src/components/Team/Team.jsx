9cb411f670d6da350a6e4b28a909742de515fcf6
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-06-20 15:46:48 +0530

    bugfix

---
+++ my-app/src/components/Team/Team.jsx
@@ -0,0 +1,129 @@
+import  "./team.scss"
+
+const Team = () =>{
+    return(
+        <div className="TeamParent">
+            <div className="container">
+  <div className="d-flex">
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/srishti.jpeg" alt=""/>
+          </div>
+         
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Srishti Pandey</h3>
+        <small>GDSC Lead</small>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/ritu.jpg" alt=""/>
+          </div>
+          
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Ritu Bhangale</h3>
+        <small>Design Lead</small>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/Deep.jpg" alt=""/>
+          </div>
+          
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Deep Panchani</h3>
+        <small>Web Dev Lead</small>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/ml.jpg" alt=""/>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Raj Aryan</h3>
+        <small>App Dev Lead</small>
+      </div>
+    </div>
+    
+  </div>
+</div>
+<div className="container2">
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/ml.jpg" alt=""/>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Raj Aryan</h3>
+        <small>App Dev Lead</small>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/drock.jpg" alt=""/>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Divyansh Kumar</h3>
+        <small>ML Lead</small>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-img">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="images/ambuj.jpeg" alt=""/>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="card-body">
+        <h3>Ambuj Raj</h3>
+        <small>Management Lead</small>
+      </div>
+    </div>
+    </div>
+        </div>
+    )
+}
+
+export default Team
