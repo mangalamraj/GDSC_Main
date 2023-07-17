8ab4e4a6e4deec9f91a1e35750125c688df5a119
ambujraj2001 <76392900+ambujraj2001@users.noreply.github.com>
2023-07-17 23:00:59 +0530

    changes in footer

--- my-app/src/components/Crew/Crew.jsx
+++ my-app/src/components/Crew/Crew.jsx
@@ -2,73 +2,75 @@ import styles from "./Crew.module.scss";
 
 const Crew = () => {
   return (
-    <div  className={styles.CrewKabaap} id="crew">
+    <div className={styles.CrewKabaap} id="crew">
 
-    <div className={styles.body}>
-      <header>
-        <div className={styles.title}>
-          <h3>The creative crew</h3>
-        </div>
-        <div className={styles.content}>
-          <h5>who we are</h5>
-          <p>
-            We are team of creatively diverse. driven. innovative individuals
-            working in various locations from the world.
-          </p>
-        </div>
-      </header>
+      <div className={styles.body}>
+        <header>
+          <div className={styles.title}>
+            <h3>The creative crew</h3>
+          </div>
+          <div className={styles.content}>
+            <h5>who we are</h5>
+            <p>
+              We are team of creatively diverse. driven. innovative individuals
+              working in various locations from the world.
+            </p>
+          </div>
+        </header>
+        <main>
+          <div className={styles.profile}>
+            <figure data-value="GDSC Lead">
+              <img src="images/srishti.jpeg" alt="" />
+              <figcaption style={{ marginBottom: "50px" }}>Srishti Pandey</figcaption>
+            </figure>
+          </div>
+        </main>
+        <main>
+        
 
-      <main>
-        <div className={styles.profile}>
-          <figure data-value="GDSC Lead">
-            <img src="images/srishti.jpeg" alt="" />
-            <figcaption style={{marginBottom:"50px"}}>Srishti Pandey</figcaption>
-          </figure>
-        </div>
+          <div className={styles.profile}>
+            <figure data-value="Web Dev Lead">
+              <img src="images/Deep.jpg" alt="" />
+              <figcaption>Deep Panchani</figcaption>
+            </figure>
+          </div>
 
-        <div className={styles.profile}>
-          <figure data-value="Web Dev Lead">
-            <img src="images/Deep.jpg" alt="" />
-            <figcaption>Deep Panchani</figcaption>
-          </figure>
-        </div>
+          <div className={styles.profile}>
+            <figure data-value="Design Lead">
+              <img src="images/ritu.jpg" alt="" height="350px" />
+              <figcaption style={{ marginBottom: "50px" }}>Ritu Bhangale</figcaption>
+            </figure>
+          </div>
 
-        <div className={styles.profile}>
-          <figure data-value="Design Lead">
-            <img src="images/ritu.jpg" alt=""  height="350px" />
-            <figcaption style={{marginBottom:"50px"}}>Ritu Bhangale</figcaption>
-          </figure>
-        </div>
+          <div className={styles.profile}>
+            <figure data-value="APP Dev Lead">
+              <img src="images/ml.jpg" alt="" height="350px" />
+              <figcaption>Raj Aryan</figcaption>
+            </figure>
+          </div>
 
-        <div className={styles.profile}>
-          <figure data-value="APP Dev Lead">
-            <img src="images/ml.jpg"  alt="" height="350px"/>
-            <figcaption>Raj Aryan</figcaption>
-          </figure>
-        </div>
+          <div className={styles.profile}>
+            <figure data-value="Management Lead">
+              <img src="images/ambuj.jpeg" alt="" height="350px" />
+              <figcaption>AMBUJ RAJ</figcaption>
+            </figure>
+          </div>
 
-        <div className={styles.profile}>
-          <figure data-value="Management Lead">
-            <img src="images/ambuj.jpeg" alt="" height="350px"/>
-            <figcaption>AMBUJ RAJ</figcaption>
-          </figure>
-        </div>
+          <div className={styles.profile} id="profile">
+            <figure data-value="ML Lead">
+              <img src="images/drock.jpg" alt="" height="300px" />
+              <figcaption>Divyansh Kumar</figcaption>
+            </figure>
+          </div>
+          <div className={styles.profile} id="profile2">
+            <figure data-value="Content Lead">
+              <img src="images/chetan.jpeg" alt="" height="300px" />
+              <figcaption>Chetan Amritanshu</figcaption>
+            </figure>
+          </div>
+        </main>
 
-        <div className={styles.profile} id="profile">
-          <figure data-value="ML Lead">
-            <img src="images/drock.jpg" alt="" height="300px"/>
-            <figcaption>Divyansh Kumar</figcaption>
-          </figure>
-        </div>
-        <div className={styles.profile} id="profile2">
-          <figure data-value="Content Lead">
-            <img src="images/chetan.jpeg" alt="" height="300px"/>
-            <figcaption>Chetan Amritanshu</figcaption>
-          </figure>
-        </div>
-      </main>
-
-    </div>
+      </div>
     </div>
   );
 };
--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -6,41 +6,41 @@ import { faFacebookF, faTwitter, faLinkedinIn, faGooglePlusG } from '@fortawesom
 
 const Footer = () => {
   return (
-    
-        <div className="foot" id='footer' style={{marginTop:"100px"}}>
-        <div align="right" className="logo">
-    <img src="images/Logo/color/left/name.svg"></img>
- </div>
- <div className="head_section">
-Let's Build Something Great Together
- </div>
- <div className="mid_section">
- <div className="sub_head">
-    
-    </div>
-    <div className="details">
-    <div className="sub_details">
-    <div className="details1">
-    <p>IIIT Nagpur</p>
-    <p> Nagpur, Maharashtra</p>
-    </div>
-    <div className="details2">
-    <p>+91 9430501530</p>
-    <p>mango.26june@gmail.com</p>
-    </div>
-    </div>
-    <div className="details3">
-    "Empowering students through technology and fostering a collaborative community at GDSC IIIT Nagpur."<br/>
-"Exploring innovation, learning, and growth together at GDSC IIIT Nagpur."
-    </div>
-    </div>
- </div>
 
- <div className="end_section">
- <div className="contact_box" >
+    <div className="foot" id='footer' style={{ marginTop: "100px" }}>
+      <div align="right" className="logo">
+        <img src="images/Logo/color/left/name.svg"></img>
+      </div>
+      <div className="head_section">
+        Let's Build Something Great Together
+      </div>
+      <div className="mid_section">
+        <div className="sub_head">
 
-<div className="contact_menu">
-{/*  * <div className= "icons">
+        </div>
+        <div className="details">
+          <div className="sub_details">
+            <div className="details1">
+              <p>IIIT Nagpur</p>
+              <p> Nagpur, Maharashtra</p>
+            </div>
+            <div className="details2">
+              <p>+91 91696 51969</p>
+              <p>gdsc@iiitn.ac.in</p>
+            </div>
+          </div>
+          <div className="details3">
+            "Empowering students through technology and fostering a collaborative community at GDSC IIIT Nagpur."<br />
+            "Exploring innovation, learning, and growth together at GDSC IIIT Nagpur."
+          </div>
+        </div>
+      </div>
+
+      <div className="end_section">
+        <div className="contact_box" >
+
+          <div className="contact_menu">
+            {/*  * <div className= "icons">
     <a href="/contact"><FontAwesomeIcon icon={faFacebookF}/></a>
     </div>
   <div className= "icons">
@@ -52,41 +52,41 @@ Let's Build Something Great Together
   <div className= "icons">
     <a href="/contact"><FontAwesomeIcon icon={faGooglePlusG}/></a>
   </div>*/}
-  
-</div>
-</div>
 
-<div align="right" className= "foot_section">
+          </div>
+        </div>
+
+        <div align="right" className="foot_section">
           <div className='foot_item'>
 
-              Company
-        
+            Company
+
           </div>
           <div
             className='foot_item'
-          
+
           >
 
-              Careers
-           
-           
-          </div>
-          <div className='foot_item'>
+            Careers
 
-              Services
 
           </div>
           <div className='foot_item'>
 
-              Projects
-           
+            Services
+
           </div>
-</div>
-</div>
+          <div className='foot_item'>
 
+            Projects
 
+          </div>
         </div>
-    
+      </div>
+
+
+    </div>
+
   )
 }
 
